56aea9296ca46e04ea539ebe1e017f93a41001ae
stratis-vip <stratis.vip@gmail.com>
2020-04-26 18:55:00 +0300

    delete dist folder

--- dist/models/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createApiResponse = function (status, datac, code) {
-    if (code === void 0) { code = 200; }
-    return { status: status, datac: datac, code: code };
-};
-//# sourceMappingURL=utils.js.map
